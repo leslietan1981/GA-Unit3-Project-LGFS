917fa51981d5e588f85aa0d45f44d571ea35a71c
leslietan1981 <leslietan.yw1981@gmail.com>
2026-05-21 15:21:50 +0800

    Add structure and base server.js and db.js

---
+++ backend/server.js
@@ -0,0 +1,46 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import connectDB from "./src/db/db.js";
+// import authRouter from "./src/routers/authRouter.js";
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use((err, req, res, next) => {
+  if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
+    console.error("JSON parsing error:", err.message);
+    return res.status(400).json({
+      status: 400,
+      message: "invalid JSON format",
+    });
+  } else if (err instanceof SyntaxError && err.status === 400 && err.type === "entity.parse.failed") {
+    console.error("URL-encoded parsing error:", err.message);
+    return res.status(400).json({
+      status: 400,
+      message: "invalid form data format",
+    });
+  }
+
+  next(err);
+});
+
+// app.use("/api", authRouter);
+
+app.listen(5001);
+
+app.use((err, req, res, next) => {
+  console.error("Unhandled error:", err);
+  console.error(err.message);
+  console.error(err.stack);
+
+  res.status(err.status || 500).json({
+    status: "error",
+    message: err.message,
+  });
+});
