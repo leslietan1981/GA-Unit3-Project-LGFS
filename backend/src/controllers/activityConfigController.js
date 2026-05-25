6efdd4ff032be6ae066bf3e665250e6564f83252
leslietan1981 <leslietan.yw1981@gmail.com>
2026-05-25 13:35:31 +0800

    Add controller and router to create new config

--- backend/src/controllers/activityConfigController.js
+++ backend/src/controllers/activityConfigController.js
@@ -1,6 +1,6 @@
 import express from "express";
 import ActivityConfigModel from "../models/ActivityConfigModel.js";
-import { setErrorObj } from "../utils/appUtils.js";
+import { getErrorObj, setErrorObj } from "../utils/appUtils.js";
 
 export const getActivityConfig = async (req, res, next) => {
   try {
@@ -9,5 +9,26 @@ export const getActivityConfig = async (req, res, next) => {
     res.json(configs);
   } catch (error) {
     return next(setErrorObj(error, 400, "failed to get activity configurations"));
+  }
+};
+
+export const createActivityConfig = async (req, res, next) => {
+  try {
+    const activityFound = await ActivityConfigModel.findOne({ type: req.body.type });
+    if (activityFound) {
+      return next(getErrorObj(409, "activity name already exists"));
+    }
+
+    await ActivityConfigModel.create({
+      type: req.body.type,
+      distance_m_toggle: req.body.distance_m_toggle,
+      duration_ms_toggle: req.body.duration_ms_toggle,
+      laps_toggle: req.body.laps_toggle,
+      intensity_level_toggle: req.body.intensity_level_toggle,
+    });
+
+    res.json("activity configuration created");
+  } catch (error) {
+    return next(setErrorObj(error, 400, "failed to create activity configuration"));
   }
 };
--- backend/src/routers/activityConfigRouter.js
+++ backend/src/routers/activityConfigRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getActivityConfig } from "../controllers/activityConfigController.js";
+import { createActivityConfig, getActivityConfig } from "../controllers/activityConfigController.js";
 
 const router = express.Router();
 
 router.get("/admin", getActivityConfig);
+router.put("/admin", createActivityConfig);
 
 export default router;
