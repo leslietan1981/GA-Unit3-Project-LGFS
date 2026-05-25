a14300278da92d13b714dcd69e0e4f7f442b51a5
leslietan1981 <leslietan.yw1981@gmail.com>
2026-05-25 12:53:13 +0800

    Add validator for createRecordedActivity controller

--- backend/src/routers/recordedActivityRouter.js
+++ backend/src/routers/recordedActivityRouter.js
@@ -7,11 +7,13 @@ import {
   getRecordedActivityById,
   updateRecordedActivityById,
 } from "../controllers/recordedActivityController.js";
+import { checkCreateRecordedActivity } from "../validators/recordedActivityValidator.js";
+import { checkErrors } from "../validators/checkErrors.js";
 
 const router = express.Router();
 
 router.get("/list", getActivityTypes);
-router.put("/recorded", createRecordedActivity);
+router.put("/recorded", checkCreateRecordedActivity, checkErrors, createRecordedActivity);
 router.get("/recorded", getRecordedActivities);
 router.post("/recorded", getRecordedActivityById);
 router.patch("/recorded", updateRecordedActivityById);
