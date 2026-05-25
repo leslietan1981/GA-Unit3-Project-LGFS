e2c1140d6335f1dfc2b4dc22ceae91a8fcf5b880
leslietan1981 <leslietan.yw1981@gmail.com>
2026-05-25 15:02:57 +0800

    Add controller and router to update activity configuration

--- backend/src/controllers/activityConfigController.js
+++ backend/src/controllers/activityConfigController.js
@@ -2,16 +2,6 @@ import express from "express";
 import ActivityConfigModel from "../models/ActivityConfigModel.js";
 import { getErrorObj, setErrorObj } from "../utils/appUtils.js";
 
-export const getActivityConfig = async (req, res, next) => {
-  try {
-    const configs = await ActivityConfigModel.find();
-
-    res.json(configs);
-  } catch (error) {
-    return next(setErrorObj(error, 400, "failed to get activity configurations"));
-  }
-};
-
 export const createActivityConfig = async (req, res, next) => {
   try {
     const activityFound = await ActivityConfigModel.findOne({ type: req.body.type });
@@ -31,5 +21,53 @@ export const createActivityConfig = async (req, res, next) => {
     res.json("activity configuration created");
   } catch (error) {
     return next(setErrorObj(error, 400, "failed to create activity configuration"));
+  }
+};
+
+export const getActivityConfig = async (req, res, next) => {
+  try {
+    const configs = await ActivityConfigModel.find();
+
+    res.json(configs);
+  } catch (error) {
+    return next(setErrorObj(error, 400, "failed to get activity configurations"));
+  }
+};
+
+export const getActivityConfigById = async (req, res, next) => {
+  try {
+    const configFound = await ActivityConfigModel.findById(req.body.activity_config_id);
+    if (!configFound) {
+      return next(getErrorObj(404, "activity configuration not found"));
+    }
+
+    res.json(configFound);
+  } catch (error) {
+    return next(setErrorObj(error, 400, "failed to get activity configuration"));
+  }
+};
+
+export const updateActivityConfigById = async (req, res, next) => {
+  try {
+    const configFound = await ActivityConfigModel.findOne({ type: req.body.type });
+    if (configFound) {
+      return next(getErrorObj(409, "activity name already exists"));
+    }
+
+    const updatedConfig = await ActivityConfigModel.findByIdAndUpdate(req.body.activity_config_id, {
+      type: req.body.type,
+      distance_m_toggle: req.body.distance_m_toggle,
+      duration_ms_toggle: req.body.duration_ms_toggle,
+      laps_toggle: req.body.laps_toggle,
+      intensity_level_toggle: req.body.intensity_level_toggle,
+      comments_toggle: req.body.comments_toggle,
+    });
+    if (!updatedConfig) {
+      return next(getErrorObj(404, "activity config not found"));
+    }
+
+    res.json("activity updated");
+  } catch (error) {
+    return next(setErrorObj(error, 400, "failed to update activity configuration"));
   }
 };
--- backend/src/routers/activityConfigRouter.js
+++ backend/src/routers/activityConfigRouter.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { createActivityConfig, getActivityConfig } from "../controllers/activityConfigController.js";
+import {
+  createActivityConfig,
+  getActivityConfig,
+  getActivityConfigById,
+  updateActivityConfigById,
+} from "../controllers/activityConfigController.js";
 
 const router = express.Router();
 
-router.get("/admin", getActivityConfig);
 router.put("/admin", createActivityConfig);
+router.get("/admin", getActivityConfig);
+router.post("/admin", getActivityConfigById);
+router.patch("/admin", updateActivityConfigById);
 
 export default router;
