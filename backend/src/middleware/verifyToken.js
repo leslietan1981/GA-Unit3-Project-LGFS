50ccc47bbd4bd9c86441df79a52c533bfd05f5a7
Genius-farmer <76843221+Genius-farmer@users.noreply.github.com>
2026-05-28 10:55:59 +0800

    update 4

--- backend/src/middleware/verifyToken.js
+++ backend/src/middleware/verifyToken.js
@@ -4,7 +4,9 @@ const verifyToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
 
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).json({ status: "error", message: "Access token missing" });
+    return res
+      .status(401)
+      .json({ status: "error", message: "Access token missing" });
   }
 
   const token = authHeader.split(" ")[1];
@@ -12,9 +14,12 @@ const verifyToken = (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.ACCESS_SECRET);
     req.user = decoded;
+    console.log("Verify successful");
     next();
   } catch (err) {
-    return res.status(401).json({ status: "error", message: "Access token invalid or expired" });
+    return res
+      .status(401)
+      .json({ status: "error", message: "Access token invalid or expired" });
   }
 };
 
