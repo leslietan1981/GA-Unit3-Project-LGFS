2f213f68d4908c5d1aac031cc360c9cce8bf1819
leslietan1981 <leslietan.yw1981@gmail.com>
2026-05-29 10:30:10 +0800

    Remove commented codes

--- backend/src/models/RecordedActivityModel.js
+++ backend/src/models/RecordedActivityModel.js
@@ -1,10 +1,7 @@
 import mongoose from "mongoose";
 
 export const RecordedActivitySchema = new mongoose.Schema(
-  // your old code, i run my code with this line. hence, i edit. Please remove my new code if you want to run your old code.
-  // const RecordedActivitySchema = new mongoose.Schema(
   {
-    // user_id: { type: mongoose.Schema.Types.ObjectId, required: true },
     type: { type: String, required: true },
     activity_date: { type: Date, required: true },
     distance_m: { type: Number },
@@ -16,5 +13,3 @@ export const RecordedActivitySchema = new mongoose.Schema(
   },
   { collection: "recordedActivities" },
 );
-
-// export default mongoose.model("RecordedActivity", RecordedActivitySchema);
